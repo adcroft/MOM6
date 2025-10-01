4622d6a0f9fb6f09ba859387a176ff5320054cee
Robert Hallberg <Robert.Hallberg@noaa.gov>
2025-10-01 10:48:32 -0400

    +*Fix 3-equation ice-ocean flux iteration (#972)
    Fix the 3-equation iteration for the buoyancy flux between the ocean and an
    overlying ice-shelf when ICE_SHELF_BUOYANCY_FLUX_ITT_BUGFIX is true and
    SHELF_3EQ_GAMMA it false.  This code now uses proper bounding of the
    self-consistent solution, avoiding further amplifying the fluxes in the cases
    when the differences between the diffusivities of heat and salt to make the
    buoyancy flux destabilizing for finite turbulent mixing.  Both the
    false-position iterations and the (appropriately chosen) Newton's method
    iterations have been extensively examined and determined to be working correctly
    via print statements that have subsequently been removed for efficiency.
    
      Previously, the code to determine the 3-equation solution for the buoyancy
    flux between the ocean and an ice shelf had been skipping iteration altogether
    or doing un-bounded Newton's method iterations with a sign error in part of the
    derivative, including taking the square root of negative numbers, leading to the
    issue described at https://github.com/NOAA-GFDL/MOM6/issues/945.  That issue has
    now been corrected and can be closed once this commit has been merged into
    the dev/gfdl branch of MOM6.
    
      This commit also changes the names of the runtime parameters to correct the
    ice shelf flux iteration bugs from ICE_SHELF_BUOYANCY_FLUX_ITT_BUG and
    ICE_SHELF_SALT_FLUX_ITT_BUG to ICE_SHELF_BUOYANCY_FLUX_ITT_BUGFIX and
    ICE_SHELF_SALT_FLUX_ITT_BUGFIX to avoid confusion with other ..._BUG parameters
    where `true` is to turn the bugs on, whereas here `true` fixes them.  The old
    names are retained via `old_name` arguments to the `get_param()` calls, so no
    existing configurations will be disrupted by these changes.
    
      Additionally, an expression to determine a scaling factor to limit ice-shelf
    bottom slopes in `calc_shelf_driving_stress()` was refactored to avoid the
    possibility of division by zero.
    
      This commit will change (and correct) answers for cases with
    ICE_SHELF_BUOYANCY_FLUX_ITT_BUGFIX set to true, but as these would often fail
    with a NaN from taking the square root of a negative value, it is very unlikely
    that any such configurations are actively being used, and there seems little
    point in retaining the previous answers.  No answers are changed in cases that
    do not use an active ice shelf.
    
    Co-authored-by: Alistair Adcroft <adcroft@users.noreply.github.com>

--- src/ice_shelf/MOM_ice_shelf_dynamics.F90
+++ src/ice_shelf/MOM_ice_shelf_dynamics.F90
@@ -2538,7 +2538,7 @@ subroutine calc_shelf_driving_stress(CS, ISS, G, US, taudx, taudy, OD)
         endif
 
         if (CS%max_surface_slope>0) then
-          scale = min(CS%max_surface_slope/sqrt((sx**2)+(sy**2)),1.0)
+          scale = CS%max_surface_slope / max( sqrt((sx**2) + (sy**2)), CS%max_surface_slope )
           sx = scale*sx; sy = scale*sy
         endif
 
